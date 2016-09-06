7c5603f10a99fb351893d2ddcfc8c9fb7fa65774
litian <tify5236@qq.com>
2016-09-06 12:57:56 +0800

    修改状态栏为透明

--- app/src/main/java/com/phone1000/groupproject/MainActivity.java
+++ app/src/main/java/com/phone1000/groupproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.phone1000.groupproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //透明导航栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 }
