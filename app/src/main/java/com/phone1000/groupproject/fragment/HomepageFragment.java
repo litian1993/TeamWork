2ae2099e8c1f5ab92e7053cd5efc13611ee570da
litian <tify5236@qq.com>
2016-09-07 16:20:15 +0800

    teamwork首次提交

---
+++ app/src/main/java/com/phone1000/groupproject/fragment/HomepageFragment.java
@@ -0,0 +1,20 @@
+package com.phone1000.groupproject.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ${USER_NAME} on 2016/9/7.
+ */
+public class HomepageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+    }
+}
